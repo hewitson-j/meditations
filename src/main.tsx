6c266b2c81fced7598ae418d1f272a31601b175a
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-11-25 22:42:16 -0700

    Added particlebg

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ParticlesBg from "particles-bg";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ParticlesBg type="cobweb" bg />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
