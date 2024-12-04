5abc6aff5a8d55b5c24e4761b22836d39afcd296
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-12-03 21:44:57 -0700

    Added entry page, test data

--- src/components/pages/EntryPage.tsx
+++ src/components/pages/EntryPage.tsx
@@ -1,8 +1,51 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./EntryPage.css";
+import { testEntries } from "./testData";
+import Header from "../shared/Header";
+import PageHead from "../shared/PageHead";
 
 export default function EntryPage() {
   const { id } = useParams();
-  console.log(id);
-  return <>Entry {id}</>;
+  const navigate = useNavigate();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const entry = testEntries[parseInt(id ? id : "1000")];
+  if (!entry) navigate("/error/not-found");
+
+  return (
+    <div className="entry">
+      <PageHead title={`${entry.title}`} />
+      <div>
+        <Header size="h1">{entry.title}</Header>
+        <p>
+          Published -{" "}
+          {entry.timestamp.getDate() +
+            " " +
+            months[entry.timestamp.getMonth()] +
+            " " +
+            entry.timestamp.getFullYear()}
+        </p>
+        <div className="entry-paragraphs">
+          {entry.paragraphs.map((paragraph, index) => {
+            if (paragraph.trim() === "") return <br key={`break-${index}`} />;
+            else return <p key={`paragraph-${index}`}>{paragraph}</p>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/pages/testData.ts
@@ -0,0 +1,55 @@
+type testEntry = {
+  id: number;
+  title: string;
+  timestamp: Date;
+  paragraphs: string[];
+};
+
+export const testEntries: testEntry[] = [
+  {
+    id: 1,
+    title: "You",
+    paragraphs: [
+      "I walked along a path and ran into You.",
+      "I looked at You and you at Me,",
+      "In an instant we knew that We were We.",
+      "",
+      "It has been said that in other worlds beyond this,",
+      "Different versions of You live unique lives,",
+      "Playing unique roles, the same person but different.",
+      "",
+      `I asked You, "What was your life like?`,
+      "What was different? What was the same?",
+      "What was your family like? What role did you play?",
+      "What was your life’s work? What skills had you learned?",
+      `What would you ask yourself if yourself answered in return?"`,
+      "",
+      `"Did you have moments of joy? Did you have moments of sorrow?`,
+      "What had you learned through life? What was important to you?",
+      "What were your struggles? Did you have any regrets?",
+      "What helps did you need? Had anyone answered your requests?",
+      "Why do you think we are? What do you think is our purpose?",
+      `Why are you? And why are we?"`,
+      "",
+      "You sat and pondered for a moment, then shared with me your joys,",
+      "Your triumphs, your fears, your hopes, your tears, your pain, your love",
+      "Your life, your knowledge, and indeed you shared with me",
+      "What I would see from another perspective.",
+      "",
+      "I also shared my fears and hopes and dreams, ",
+      "My joys, my sorrows, my achievements, my struggles,",
+      "And it was in that moment when the realization struck",
+      "About what the wise one once said",
+      `"Do unto others as you would have done unto You."`,
+      "I am You, and You are Me, and We are We.",
+      "",
+      "You saw things differently, but you confirmed we are the same",
+      "You showed me that we are truly connected though living different lives",
+      "And beyond all, you showed that we need not seek to travel vast distances",
+      "To other worlds to see how we live in other lives, we need not travel so far",
+      "To see that We are One, no matter who, what, or when we are",
+      "When we see things from another’s eyes.",
+    ],
+    timestamp: new Date("7 August 2024"), //"7 August 2024",
+  },
+];
