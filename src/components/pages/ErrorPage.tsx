8b40d472fce10134fc4689a8f32518e4fa04257f
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-12-09 20:17:50 -0700

    Edited button styles

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,6 +1,6 @@
-import { useNavigate } from "react-router-dom";
 import "./ErrorPage.css";
 import PageContainer from "../shared/PageContainer";
+import NavButton from "../shared/NavButton";
 
 interface ErrorPageProps {
   title?: string;
@@ -11,20 +11,14 @@ export default function ErrorPage({
   title = "Error",
   message = "You've come to the wrong page! Click the button to return home.",
 }: ErrorPageProps) {
-  const navigate = useNavigate();
-
   return (
     <PageContainer className="error">
       <div className="error-content">
         <h1>{title}</h1>
         <p>{message}</p>
-        <button
-          onClick={() => {
-            navigate("/home");
-          }}
-        >
+        <NavButton type="navigation" to={"/home"}>
           Home
-        </button>
+        </NavButton>
       </div>
     </PageContainer>
   );
